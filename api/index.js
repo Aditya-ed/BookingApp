decb4d4862afebdd885de521894fd6840568fc8b
Aditya Rawat <142314601+Aditya-ed@users.noreply.github.com>
2024-08-15 21:20:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials:true,
-    origin:'https://booking-app-indol-delta.vercel.app',
+    origin:'http://localhost:5173',
 }));
 
 console.log(process.env.MONGO_URL);
